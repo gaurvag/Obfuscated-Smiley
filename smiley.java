46d9faf7f0ba54fec281f8aaf6aa5531dca0e505
Gaurav <gauravagrawal142000@gmail.com>
2020-07-28 10:26:34 +0530

    initial commit

---
+++ smiley.java
@@ -0,0 +1,15 @@
+/*
+* Author : Gaurav Agrawal
+* Type : Obfuscated
+* Input : Integer input,best works in 9 to 13
+*/
+/*‮*/import java.util.Scanner;
+/*‭*//*‮*/import java.util.stream.IntStream;import static java.lang.System.*;
+/*‭*//*‮*/public class GauravProgram{static String nm="\nthnx 4 trying :)\n-Gaurav Agrawal";public static void main(String..._$$){int __=3,___=__*__;try{__=new Scanner(System.in).nextInt();}catch(Exception e){makeSmiley(___);out.print("\n//see what to Input from comments in code");exit(__-__);}  /*‭*/
+/*‮*/makeSmiley(__);}static void makeSmiley(int __){int _$=__/__,__$=_$+_$,___$=_$+__$,_____$=__$+___$,____$=__$*__$*___$; IntStream.rangeClosed(-__,__).forEach(___->IntStream.rangeClosed(-__,__).forEach(____->out.print(((___+__/__$)*(___+__/__$)+(____-_____$*__/____$)*(____-_____$*__/____$)<=__*__/(____$*___$)?"  ":(___+__/__$)*(___+__/__$)+(____+__/___$)*(____+__/___$)<=__*__/(____$*___$)?"  ":___>=__/__$&&(___*___+____*____<=__*__/__$)?"  ":___<=-____*____?"@.":___*___+____*____<__*__?(____<(_$-_$)?"*.":"#."):"  ")+(____==__?"\n":"")+(___==__&&____==__?(__<(___$*___$)?"\nIt best works in range from 9 to 13"+nm:nm):""))));}}
+/*‭*/
+
+      
+      
+
+//🌚
